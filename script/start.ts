c0548fb60dee0de874b9da53dbe031e07865c6a3
Matthias Giger <i@matthiasgiger.com>
2021-03-21 16:34:16 +0100

    fix(start): build only if necessary and improve log order

--- script/start.ts
+++ script/start.ts
@@ -1,13 +1,19 @@
+import { existsSync } from 'fs'
+import { join } from 'path'
 import { spawn } from 'child_process'
 import { log } from '../helper'
 import { options } from '../options'
 import { build } from './build'
 
 export const start = () => {
-  log('starting...')
+  const entryFile = `${options().output}/index.js`
 
   // Regular build first, to ensure files for nodemon are available.
-  build()
+  if (!existsSync(join(process.cwd(), entryFile))) {
+    build()
+  }
+
+  log('starting...')
 
   // Run TSC Watcher and Server in parallel.
   const childTsc = spawn('tsc', ['--watch'], {
@@ -15,14 +21,10 @@ export const start = () => {
     cwd: process.cwd(),
   })
 
-  const childNodemon = spawn(
-    'nodemon',
-    ['-q', `${options().output}/index.js`],
-    {
-      stdio: 'inherit',
-      cwd: process.cwd(),
-    }
-  )
+  const childNodemon = spawn('nodemon', ['-q', entryFile], {
+    stdio: 'inherit',
+    cwd: process.cwd(),
+  })
 
   // Close server manually for tests.
   return () => {
